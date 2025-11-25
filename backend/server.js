cf24e3d74c3e9ff958e60ac3391fcd767417c16f
Shreyas Parab <110821677+shreyas264@users.noreply.github.com>
2025-11-25 15:26:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://gorgeous-boba-2b9349.netlify.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 // middlewares
 app.use(cors({
-  origin: "http://localhost:5173",  // allow frontend
+  origin: "https://gorgeous-boba-2b9349.netlify.app",  // allow frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
