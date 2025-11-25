e7d53e9f23dceb4c29794bda7177f1ae23412375
Shreyas Parab <110821677+shreyas264@users.noreply.github.com>
2025-11-25 13:07:03 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     setLoading(true)
     try {
       const {data} = await axios.post(
-        "http://localhost:5000/api/users/login",
+        "https://parab-chat-app.onrender.com/api/users/login",
         {
           email,
           password,
